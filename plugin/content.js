fb2f93b2ee64f3952839d3058842fee734a9e10f
nickhit3802 <62406082+nickhit3802@users.noreply.github.com>
2021-07-17 14:31:22 -0700

    Add files via upload

--- plugin/content.js
+++ plugin/content.js
@@ -1,6 +1,32 @@
-setInterval(myTimer, 1000);
+// Timer
+var theTimer  = '';
+let t = 0;
 
-function myTimer() {
-  const d = new Date();
-  document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+function startTimer(){
+  theTimer = setInterval(function () {
+    t++;
+    document.getElementById("dem").innerHTML = t;
+  }, 1000);
+}
+
+startTimer();
+
+// Storage mockup
+window.onload = function() {
+  document.getElementById('save').onclick = function() {
+    var value = document.getElementById('saveLine').value;
+    //alert(value);
+
+    // Storing user input
+    chrome.storage.sync.set({'myLine': value}, function() {
+      alert('Success!');
+    });
+  }
+
+  // Retrieving user input
+  document.getElementById('get').onclick = function() {
+    chrome.storage.sync.get('myLine', function(data) {
+      alert(data.myLine);
+    })
+  }
 }
