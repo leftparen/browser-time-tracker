d0c4df9a797eb0fe8b4070fd4d6669d9df86818b
nickhit3802 <62406082+nickhit3802@users.noreply.github.com>
2021-07-27 19:52:18 -0700

    Add files via upload
    - The plugin now stores the website data from past sessions instead of only storing the last website visited.

--- plugin/background.js
+++ plugin/background.js
@@ -1,4 +1,11 @@
-var websiteDict = {};
+if (localStorage.getItem('websiteDict')) {
+    var websiteDict = JSON.parse(localStorage.getItem('websiteDict'));
+    console.log('websiteDict does exist');
+} else {
+    var websiteDict = {};
+    console.log('websiteDict does not exist');
+};
+
 var sortedWebDict = [];
 
 // Detects when user clicks off of window
@@ -26,46 +33,51 @@ function processSiteChange() {
 
     // Gets url and hostname everytime user changes websites
     chrome.tabs.query({"active": true}, function(tabs) {
-        let url = tabs[0].url;
-        let urlObject = new URL(url);
-        let hostName = urlObject.hostname;
-        console.log(hostName);
-        if (!(hostName in websiteDict)){
-            websiteDict[hostName] = 0;
-        }
-
-        // If there was a last website, program gets it. If not, program stores current site as the last website
-        if (localStorage.getItem('lastWebsite') != null) {
-            let lastWebsite = JSON.parse(localStorage.getItem('lastWebsite'));
-            if (!(lastWebsite.website in websiteDict)){
-                websiteDict[lastWebsite.website] = 0;
+        if (tabs[0]) {
+            let url = tabs[0].url;
+            let urlObject = new URL(url);
+            let hostName = urlObject.hostname;
+            console.log(hostName);
+            if (!(hostName in websiteDict)) {
+                websiteDict[hostName] = 0;
             }
-            let secondsPassed = (Date.now() - lastWebsite.timeStamp) / 1000;
-            console.log(secondsPassed);
-            websiteDict[lastWebsite.website] = secondsPassed + websiteDict[lastWebsite.website];
-            lastWebsite = {
-                website: hostName,
-                timeStamp: Date.now()
-            }
-            localStorage.setItem('lastWebsite', JSON.stringify(lastWebsite));
-        } else {
-            let lastWebsite = {
-                website: hostName,
-                timeStamp: Date.now()
-            }
-            localStorage.setItem('lastWebsite', JSON.stringify(lastWebsite));
-        }
 
-        // Sorting the array based on their values (time spent on a site)
-        sortedWebDict = Object.keys(websiteDict).map(function (key) {
-            return [key, websiteDict[key]];
-        });
-        sortedWebDict.sort(function(first, second) {
-            return second[1] - first[1];
-          });
-        console.log(sortedWebDict);
+            // If there was a last website, program gets it. If not, program stores current site as the last website
+            if (localStorage.getItem('lastWebsite') != null) {
+                let lastWebsite = JSON.parse(localStorage.getItem('lastWebsite'));
+                if (!(lastWebsite.website in websiteDict)){
+                    websiteDict[lastWebsite.website] = 0;
+                }
+                let secondsPassed = (Date.now() - lastWebsite.timeStamp) / 1000;
+                console.log(secondsPassed);
+                websiteDict[lastWebsite.website] = secondsPassed + websiteDict[lastWebsite.website];
+                lastWebsite = {
+                    website: hostName,
+                    timeStamp: Date.now()
+                }
+                localStorage.setItem('lastWebsite', JSON.stringify(lastWebsite));
+            } else {
+                let lastWebsite = {
+                    website: hostName,
+                    timeStamp: Date.now()
+                }
+                localStorage.setItem('lastWebsite', JSON.stringify(lastWebsite));
+            }
 
-        // Storing array so popup.js can use it. Find more efficient way?
-        localStorage.setItem('sortedWebDict', JSON.stringify(sortedWebDict));
+            // Storing the website data
+            localStorage.setItem('websiteDict', JSON.stringify(websiteDict));
+
+            // Sorting the array based on their values (time spent on a site)
+            sortedWebDict = Object.keys(websiteDict).map(function (key) {
+                return [key, websiteDict[key]];
+            });
+            sortedWebDict.sort(function(first, second) {
+                return second[1] - first[1];
+            });
+            console.log(sortedWebDict);
+
+            // Storing array so popup.js can use it. Find more efficient way?
+            localStorage.setItem('sortedWebDict', JSON.stringify(sortedWebDict));
+        };
     });
 }
